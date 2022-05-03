1be3d0797f5a81818c6dd54c574d47a63a647b7d
“AniketChivhe” <aniketchivhe@gmail.com>
2022-05-04 03:07:52 +0530

    UC3 added

---
+++ UC3_PartTime.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeeWage
+{
+    internal class UC3_PartTime
+    {
+        public void xyz()
+        {
+            int FULL_TIME = 1;
+            int PART_TIME = 2;
+            int EMP_RATE_PER_HR = 20;
+
+            int empHrs = 0;
+            int empWages = 0;
+            Random random = new Random();
+
+            int empCheck = random.Next(0, 3);
+
+            Console.WriteLine("Employee status : " + empCheck);
+            if (empCheck == FULL_TIME)
+            {
+                empHrs = 8;
+            }
+            else if (empCheck == PART_TIME)
+            {
+                empHrs = 4;
+            }
+            else
+            {
+                empHrs = 0;
+            }
+            empWages = empHrs * EMP_RATE_PER_HR;
+            Console.WriteLine("Employee Wage For part time : " + empWages);
+        }
+    }
+}
