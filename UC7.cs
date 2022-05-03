306aacc2ac087dfd7371826e6da59e19be121b4c
“AniketChivhe” <aniketchivhe@gmail.com>
2022-05-04 03:36:50 +0530

    UC7 added

---
+++ UC7.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeeWage
+{
+    internal class UC7
+    {
+        static void ABC(string[] args)
+        {
+            UC6 uC6 = new UC6();
+            uC6.bbb();
+        }
+
+    }
+}
