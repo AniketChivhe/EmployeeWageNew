71d2792c38b26d25efc70d8d3e54063110a81b0a
“AniketChivhe” <aniketchivhe@gmail.com>
2022-05-15 04:22:03 +0530

    [Aniket] UC12 Added

--- Program.cs
+++ Program.cs
@@ -1,47 +1,90 @@
 ﻿using System;
 
-namespace EmployeeWage
+namespace UC12_RefractorMultipleCompanies
 {
-    class Program
+    class CompanyEmpWage
     {
-        static void Main(string[] args)
+        //Declaring Constant Variable
+        public const int FULL_TIME = 1;
+        public const int PART_TIME = 2;
+        //Creating a interface 
+        public interface ComputationEmployeeWage
         {
-            //checking employee present or not
-            int Full_Time = 1;
-            Random random = new Random();
-            int empCheck = random.Next(0, 2);
-            Console.WriteLine(empCheck);
-            if (empCheck == Full_Time)
+            void ComputeEmpWage();
+            int ComputeEmpWage(ComputeEmpWage companyEmpWage);
+        }
+        //Implements the interface in class
+        public class EmpBuilderWage : ComputationEmployeeWage
+        {
+            public const int FULL_TIME = 1;
+            public const int PART_TIME = 2;
+            private int numOfCompany = 0;
+
+            private ComputeEmpWage[] companyEmpWageArray;
+            private int totalWage;
+
+
+            public EmpBuilderWage()
             {
-                Console.WriteLine("Employee is present");
+                this.companyEmpWageArray = new ComputeEmpWage[5];
             }
-            else
+            public void addCompanyEmpWage(string company, int wagePerhour, int maxHoursPerMonth, int maxWorkingDays)
             {
-                Console.WriteLine("Employee is Absent");
+                companyEmpWageArray[this.numOfCompany] = new ComputeEmpWage(company, wagePerhour, maxHoursPerMonth, maxWorkingDays);
+                numOfCompany++;
             }
+            public void ComputeEmpWage()
+            {
+                for (int i = 0; i < numOfCompany; i++)
+                {
+                    companyEmpWageArray[i].setTotalEmpWage(this.ComputeEmpWage(this.companyEmpWageArray[i]));
+                    Console.WriteLine(this.companyEmpWageArray[i].toString());
+                }
+            }
+            public int ComputeEmpWage(ComputeEmpWage companyEmpWage)
+            {
+                //Console.WriteLine("Welcome to employee wage computation");
+                //Creating a Random Function
+                int empHours = 0, totalEmpHrs = 0, totalWorkingDays = 0;
+                //workingHrs=0;
 
 
-            //for uc2 print
-            UC2_DailyWage dailyWage = new UC2_DailyWage();
-            dailyWage.abc();
+                while (totalEmpHrs <= companyEmpWage.maxHoursPerMonth && totalWorkingDays <= companyEmpWage.maxWorkingDays)
+                {
+                    //Calling the next method in Random Class
+                    totalWorkingDays++;
+                    Random r = new Random();
+                    int empAttendance = r.Next(0, 3);
+                    switch (empAttendance)
+                    {
+                        case FULL_TIME:
+                            empHours = 8;
+                            break;
+                        case PART_TIME:
+                            empHours = 4;
+                            break;
+                        default:
+                            empHours = 0;
+                            break;
 
-            //for uc3 print
-            UC3_PartTime partTime = new UC3_PartTime();
-            partTime.xyz();
+                    }
 
-            //for uc4 print
-            UC4 uC4 = new UC4();
-            uC4.lmn();
+                    totalEmpHrs += empHours;
 
-            //for uc 5 print
-            UC5_WagesForMonth wagesForMonth = new UC5_WagesForMonth();
-            wagesForMonth.aaa();
 
-            //for uc 6 print
-            UC6 uC6 = new UC6();
-            uC6.bbb();
+                }
 
-            Console.ReadKey();
+                Console.WriteLine("Days : " + totalWorkingDays + " Emp Hours : " + empHours);
+                return totalEmpHrs * companyEmpWage.wagePerHour;
+            }
+            static void Main(string[] args)
+            {
+                Console.WriteLine("Welcome to employee wage computation - UC_12  Refactor to use ArrayList");
+                EmpBuilderWage empBuilderWage = new EmpBuilderWage();
+                empBuilderWage.addCompanyEmpWage("Suzuki", 70, 20, 10);
+                empBuilderWage.addCompanyEmpWage("Yamaha", 50, 30, 20);
+                empBuilderWage.ComputeEmpWage();
+            }
         }
     }
 }
